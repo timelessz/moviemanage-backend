68b5cc9ef9a23a0f2bd6a3451d92977366ab73a5
timelessz <834916321@qq.com>
2017-10-16 15:11:27 +0800

    电影管理后台初始化

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,10 +14,7 @@
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">-->
-
-    {{--<link rel="stylesheet" href="{{URL::asset('dist/css/bootstrap.min.css')}}">--}}
-    {{--<link rel="stylesheet" href="{{URL::asset('dist/css/bootstrap-theme.min.css')}}">--}}
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
@@ -189,9 +186,6 @@
     </div>
 </div>
 
-{{--<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"--}}
-        {{--integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"--}}
-        {{--crossorigin="anonymous"></script>--}}
 <script src="star/js/star-rating.js" type="text/javascript"></script>
 <script>
     $(function () {
