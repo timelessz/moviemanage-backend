b97e23b4b576e78f5960bc23a78e5fdc20ba62fb
timelessz <834916321@qq.com>
2017-11-19 15:16:01 +0800

    电影管理后台初始化

--- app/Http/Controllers/Index/IndexstaticController.php
+++ app/Http/Controllers/Index/IndexstaticController.php
@@ -4,9 +4,77 @@ namespace App\Http\Controllers\Index;
 
 use App\Element;
 use App\Http\Controllers\Controller;
+use App\Menu;
+use App\Movietag;
+use App\Movietype;
 
 class IndexstaticController extends Controller
 {
+
+//    1、站点访问速度
+//    2、sitemap
+//    3、爬虫爬取更新的数据
+
+    /**
+     * 用于生成站点的sitemap 等相关数据
+     * @access public
+     */
+    public function sitemap()
+    {
+        echo '<pre>';
+        $host = 'http://www.' . $_SERVER['HTTP_HOST'];
+        //首先获取全部链接的路径 从menu 中
+        $menu = (new Menu())->get_menu();
+//        print_r($menu);
+        //然后需要获取全部的电影分类 点一个标签的信息
+        $movietype = Movietype::all(['id', 'name'])->toArray();
+        $movietag = Movietag::all(['id', 'name'])->toArray();
+        array_walk($movietype, [(new Element()), 'execFormatTagType'], 'type');
+        array_walk($movietag, [(new Element()), 'execFormatTagType'], 'tag');
+        $sitemap = [];
+        foreach ($menu as $v) {
+            $sitemap[] = [
+                'loc' => $host . $v['path'],
+                'lastmod' => date('Y-m-d', time()),
+                'changefreq' => 'hourly',
+                'priority' => $v['priority'],
+            ];
+        }
+        foreach ($movietag as $v) {
+            $sitemap[] = [
+                'loc' => $host . $v['href'],
+                'lastmod' => date('Y-m-d', time()),
+                'changefreq' => 'daily',
+                'priority' => '0.6',
+            ];
+        }
+        foreach ($movietype as $v) {
+            $sitemap[] = [
+                'loc' => $host . $v['href'],
+                'lastmod' => date('Y-m-d', time()),
+                'changefreq' => 'daily',
+                'priority' => '0.6',
+            ];
+        }
+        $xml_wrapper = <<<XML
+<?xml version='1.0' encoding='utf-8'?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+</urlset>
+XML;
+        $xml = new \SimpleXMLElement($xml_wrapper);
+        foreach ($sitemap as $data) {
+            $item = $xml->addChild('url'); //使用addChild添加节点
+            if (is_array($data)) {
+                foreach ($data as $key => $row) {
+                    $item->addChild($key, $row);
+                }
+            }
+        }
+        $xmldata = $xml->asXML(); //用asXML方法输出xml，默认只构造不输出。
+        file_put_contents('sitemap.xml', $xmldata);
+    }
+
+
     //
     public function index()
     {
@@ -141,5 +209,6 @@ class IndexstaticController extends Controller
         $code = view('movielist', $element);
         return $code;
     }
+
 
 }
