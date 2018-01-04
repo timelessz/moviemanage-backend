7a4773dfe970d1961d8812e1e6573360b4888240
timelessz <834916321@qq.com>
2018-01-04 09:47:49 +0800

    修改首页样式

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,6 +45,11 @@
         s.parentNode.insertBefore(bp, s);
     })();
 </script>
+<script>(function(){
+        var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?cb130546e0e8aaffc7b3fa8261509715":"https://jspassport.ssl.qhimg.com/11.0.1.js?cb130546e0e8aaffc7b3fa8261509715";
+        document.write('<script src="' + src + '" id="sozz"><\/script>');
+    })();
+</script>
 <header class="main-header">
     <div class="container">
         <div class="row">
