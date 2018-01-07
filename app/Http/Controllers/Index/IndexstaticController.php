d00e14e456d218b96833331425c69bf27c3760de
timelessz <834916321@qq.com>
2018-01-07 12:32:15 +0800

    修改首页样式

--- app/Http/Controllers/Index/IndexstaticController.php
+++ app/Http/Controllers/Index/IndexstaticController.php
@@ -94,31 +94,34 @@ XML;
     //欧美list
     public function oumeilist($id = 1)
     {
-        return Cache::rememberForever('oumeilist' . $id, function () use ($id) {
-            $element = (new Element())->getMovieListEnsstial('oumei', $id, 20);
-            $code = view('movielist', $element);
-            return $code;
-        });
+        $element = (new Element())->getMovieListEnsstial('oumei', $id, 20);
+        $code = view('movielist', $element);
+        return $code;
+//        return Cache::rememberForever('oumeilist' . $id, function () use ($id) {
+//
+//        });
     }
 
 
     //大陆list
     public function dalulist($id = 1)
     {
+        $element = (new Element())->getMovieListEnsstial('dalu', $id, 20);
+        $code = view('movielist', $element);
+        return $code;
         return Cache::rememberForever('dalulist' . $id, function () use ($id) {
-            $element = (new Element())->getMovieListEnsstial('dalu', $id, 20);
-            $code = view('movielist', $element);
-            return $code;
+
         });
     }
 
     //rihan list
     public function rihanlist($id = 1)
     {
+        $element = (new Element())->getMovieListEnsstial('rihan', $id, 20);
+        $code = view('movielist', $element);
+        return $code;
         return Cache::rememberForever('rihanlist' . $id, function () use ($id) {
-            $element = (new Element())->getMovieListEnsstial('rihan', $id, 20);
-            $code = view('movielist', $element);
-            return $code;
+
         });
     }
 
@@ -126,20 +129,22 @@ XML;
     //港台list
     public function gangtailist($id = 1)
     {
+        $element = (new Element())->getMovieListEnsstial('gangtai', $id, 20);
+        $code = view('movielist', $element);
+        return $code;
         return Cache::rememberForever('gangtailist' . $id, function () use ($id) {
-            $element = (new Element())->getMovieListEnsstial('gangtai', $id, 20);
-            $code = view('movielist', $element);
-            return $code;
+
         });
     }
 
     //经典电影list
     public function jingdianlist($id = 1)
     {
+        $element = (new Element())->getMovieListEnsstial('jingdian', $id, 20);
+        $code = view('movielist', $element);
+        return $code;
         return Cache::rememberForever('gangtailist' . $id, function () use ($id) {
-            $element = (new Element())->getMovieListEnsstial('jingdian', $id, 20);
-            $code = view('movielist', $element);
-            return $code;
+
         });
     }
 
