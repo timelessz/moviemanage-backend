ce60a82013a0cf25ab45dae86bdfc4620efe57c3
timelessz <834916321@qq.com>
2018-01-11 23:00:56 +0800

    修改首页样式

--- resources/views/reviewdetail.blade.php
+++ resources/views/reviewdetail.blade.php
@@ -24,6 +24,27 @@
                         <h2 class="title">{{$moviereview['title']}}</h2>
                         <div class="content review-container">
                             {!!$moviereview['content'] !!}
+                            <div class="bdsharebuttonbox">
+                                <a href="#" class="bds_more" data-cmd="more"></a>
+                                <a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a>
+                                <a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a>
+                                <a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a>
+                                <a href="#" class="bds_sqq" data-cmd="sqq" title="分享到QQ好友"></a>
+                                <a href="#" class="bds_tieba" data-cmd="tieba" title="分享到百度贴吧"></a>
+                                <a href="#" class="bds_bdhome" data-cmd="bdhome" title="分享到百度新首页"></a>
+                            </div>
+                            <script>window._bd_share_config = {
+                                    "common": {
+                                        "bdSnsKey": {},
+                                        "bdText": "",
+                                        "bdMini": "1",
+                                        "bdMiniList": false,
+                                        "bdPic": "",
+                                        "bdStyle": "1",
+                                        "bdSize": "16"
+                                    }, "share": {}
+                                };
+                                with (document) 0[(getElementsByTagName('head')[0] || body).appendChild(createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=' + ~(-new Date() / 36e5)];</script>
                         </div>
                     </div>
                 </div>
@@ -42,7 +63,8 @@
                         <div class="content recent-post">
 
                             <div class="recent-single-post">
-                                <a href="{{$relative_movie['href']}}" class="post-title" title="{{$relative_movie['title']}}">
+                                <a href="{{$relative_movie['href']}}" class="post-title"
+                                   title="{{$relative_movie['title']}}">
                                     {{$relative_movie['title']}}
                                 </a>
                                 <div class="date pull-right">{{$relative_movie['created_at']}}</div>
